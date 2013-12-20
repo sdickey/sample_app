ef585f8790ff7892b7e6d61ef436806bc9a892ef
Sean Dickey <sean.dickey@live.com>
2013-12-20 14:14:08 -0800

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -21,6 +21,18 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationships_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
       describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -64,6 +76,16 @@ describe "Authentication" do
         describe "submitting to the update  action" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visitng the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title ('Sign in') }
         end
       end
     end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,6 +38,18 @@ describe "StaticPages" do
     	it "should render the user's feed" do
     		user.feed.each do |item|
     			expect(page).to have_selector("li##{item.id}", text: item.content)
+    		end
+    	end
+
+    	describe "follower/following counts" do
+    		let(:other_user) { FactoryGirl.creae(:user) }
+    		before do
+    			other_user.follow!(user)
+    			visit root_path
+    		end
+
+    		it { should have_link("0 following", href: following_user_path(user)) }
+    		it { should have_link("1 followers", href: followers_user_path(user)) }
     	end
     end
   end
